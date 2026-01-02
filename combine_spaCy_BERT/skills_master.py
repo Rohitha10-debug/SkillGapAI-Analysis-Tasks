55b42f3236febb4d3b441b25053bb7e4cb0506e0
Rohitha10-debug <rohithawork10@gmail.com>
2026-01-02 19:09:39 +0530

    Create skills_master.py

---
+++ combine_spaCy_BERT/skills_master.py
@@ -0,0 +1,11 @@
+TECHNICAL_SKILLS = [
+    "python", "java", "sql", "machine learning",
+    "data analysis", "deep learning", "nlp",
+    "spacy", "pandas", "numpy", "tensorflow"
+]
+
+SOFT_SKILLS = [
+    "communication", "teamwork", "leadership",
+    "problem solving", "time management",
+    "critical thinking"
+]
