9dcb56f2dcd4e21b738de9cc36c49eda95c91812
Rohitha10-debug <rohithawork10@gmail.com>
2026-01-02 18:46:17 +0530

    Add skills list

---
+++ milestone2/skills.py
@@ -0,0 +1,24 @@
+TECHNICAL_SKILLS = [
+    "python",
+    "java",
+    "sql",
+    "machine learning",
+    "data analysis",
+    "nlp",
+    "spacy",
+    "pandas",
+    "numpy",
+    "tensorflow",
+    "keras",
+    "scikit-learn"
+]
+
+SOFT_SKILLS = [
+    "communication",
+    "teamwork",
+    "leadership",
+    "problem solving",
+    "time management",
+    "critical thinking",
+    "adaptability"
+]
